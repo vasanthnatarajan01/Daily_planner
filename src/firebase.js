565148c73da1da954cfb1e6d0f0429db80dab3f0
Vasanth Natarajan <vasanthnatarajan01@gmail.com>
2026-05-10 22:54:47 +0530

    Add Firebase backend configuration

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1yMV5f3wJ1EcimBE9z15SJUpgdJHVcUg",
+  authDomain: "dailyplanner-35f5f.firebaseapp.com",
+  projectId: "dailyplanner-35f5f",
+  storageBucket: "dailyplanner-35f5f.firebasestorage.app",
+  messagingSenderId: "229581916924",
+  appId: "1:229581916924:web:ed3a0a8d278e043460e579"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
